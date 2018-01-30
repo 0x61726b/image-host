85dbbcc3f68649d8e7e450a43093a730def50766
arkenthera <arkenthera@gmail.com>
2018-01-30 13:14:31 +0300

    Add log

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def save_upload(r_file_name, ext):
 
 @app.before_request
 def log_request_info():
-    app.logger.info('Headers: %s', request.headers)
-    app.logger.info('Body: %s', request.get_data())
+    print('Headers: %s', request.headers)
+    print('Body: %s', request.get_data())
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
